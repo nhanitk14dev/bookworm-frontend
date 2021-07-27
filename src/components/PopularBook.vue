1c447850762b299ff09ed6bb5a4aa5e2e379aa1b
--add <nhanitk14dev@gmail.com>
2021-07-27 14:02:16 +0700

    Update singlebookcommon to Popularbook and recommendedbook

--- src/components/PopularBook.vue
+++ src/components/PopularBook.vue
@@ -1,16 +1,19 @@
 <template>
   <b-container class="bv-example-row">
     <b-row>
-      <b-col>col</b-col>
-      <b-col>col</b-col>
-      <b-col>col</b-col>
-      <b-col>col</b-col>
+      <b-col v-for="index in 4" :key="index" lg="3" md="6" sm="12">
+        <SingleBookCommon />
+      </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
+import SingleBookCommon from "@/components/SingleBookCommon.vue";
 export default {
   name: "PopularBook",
+  components: {
+    SingleBookCommon,
+  },
 };
 </script>
