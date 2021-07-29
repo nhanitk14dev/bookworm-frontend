7d559c585beed869584a6e864109a1c8d1294438
Nhan David <nhanitk14dev@gmail.com>
2021-07-30 03:04:27 +0700

    Remove state redundant in silder home

--- src/components/SliderHome.vue
+++ src/components/SliderHome.vue
@@ -48,18 +48,6 @@ export default {
           items: 4,
         },
       },
-      artists: [
-       {name: 'Davido', genre: 'afrobeats', country: 'Nigeria'},
-       {name: 'Burna Boy', genre: 'afrobeats', country: 'Nigeria'},
-       {name: 'AKA', genre: 'hiphop', country: 'South-Africa'},
-       {name: 'Sarkodie', genre: 'hiphop', country: 'Ghana'},
-       {name: 'Stormzy', genre: 'hiphop', country: 'United Kingdom'},
-       {name: 'Lil Nas', genre: 'Country', country: 'United States'},
-       {name: 'Nasty C', genre: 'hiphop', country: 'South-Africa'},
-       {name: 'Shatta-walle', genre: 'Reagae', country: 'Ghana'},
-       {name: 'Khalid', genre: 'pop', country: 'United States'},
-       {name: 'ed-Sheeran', genre: 'pop', country: 'United Kingdom'}
-      ]
     };
   },
   computed: mapState({
